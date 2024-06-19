d2f6d656365c3b7b1b45254b04368909cb5ac9e3
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2024-06-19 11:53:26 -0500

    cambio de cliente

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,14 +1,31 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const db = new Sequelize({
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   dialect: "postgres",
+//   logging: false,
+// });
+
+// module.exports = db;
+
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
+const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Necesario para evitar errores con certificados SSL auto-firmados
+    },
+  },
 });
 
 module.exports = db;
