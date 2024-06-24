a9f48d429e95fbc1f02f7fabf02b2e491263185b
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2024-06-23 22:08:49 -0500

    delete port

--- src/controllers/PortsBad.controllers.js
+++ src/controllers/PortsBad.controllers.js
@@ -35,8 +35,7 @@ const PortsBadActualize = async (req, res) => {
 const PortsBadEliminate = async (req, res) => {
   try {
     const { id } = req.params;
-    const port = req.body;
-    const result = await PortsBadServices.delete(id, port);
+    const result = await PortsBadServices.delete(id);
     res.status(200).json({ message: "Usuario eliminado con éxito" });
   } catch (error) {
     res.status(400).json(error.message);
